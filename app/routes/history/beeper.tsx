65818b0625890ed36406dd9441905ea07c7dcc5d
Banks Nussman <banks@nussman.us>
2021-06-27 21:51:47 -0400

    Refactored Beeps and Ratings per user

--- app/routes/history/beeper.tsx
+++ app/routes/history/beeper.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { Layout, Text, Divider, List, ListItem, Spinner } from '@ui-kitten/components';
+import React, {useContext} from 'react';
+import { Layout, Text, Divider, List, ListItem, Spinner, TopNavigationAction, TopNavigation } from '@ui-kitten/components';
 import { StyleSheet } from 'react-native';
 import ProfilePicture from '../../components/ProfilePicture';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 import { MainNavParamList } from '../../navigators/MainTabs';
 import { gql, useQuery } from '@apollo/client';
-import { GetBeepHistoryQuery } from '../../generated/graphql';
+import { GetBeepHistoryQuery, Beep } from '../../generated/graphql';
+import {UserContext} from '../../utils/UserContext';
+import {BackIcon} from '../../utils/Icons';
 
 interface Props {
     navigation: BottomTabNavigationProp<MainNavParamList>;
 }
 
 const GetBeepHistory = gql`
-    query GetBeepHistory {
-        getBeepHistory {
+    query GetBeepHistory($id: String) {
+        getBeeps(id: $id) {
             items {
                 id
                 start
@@ -28,66 +30,97 @@ const GetBeepHistory = gql`
                     last
                     photoUrl
                 }
+                beeper {
+                    id
+                    name
+                    first
+                    last
+                    photoUrl
+                }
             }
             count
         }
     }
 `;
 
-export function BeeperRideLogScreen(props: Props) {
-    const { data, loading, error, refetch } = useQuery<GetBeepHistoryQuery>(GetBeepHistory);
+export function BeepsScreen(props: Props) {
+    const user = useContext(UserContext);
+    const { data, loading } = useQuery<GetBeepHistoryQuery>(
+        GetBeepHistory,
+        { variables: { id: user.id } }
+    );
 
-        const renderItem = ({ item }) => (
+    const BackAction = () => (
+        <TopNavigationAction icon={BackIcon} onPress={() => props.navigation.goBack()}/>
+    );
+
+    const renderItem = ({ item }: { item: Beep }) => {
+        const otherUser = user.id === item.rider.id ? item.beeper : item.rider;
+        return (
             <ListItem
-                accessoryLeft={() => {
-                    return (
+                accessoryLeft={() =>
+                    (
                         <ProfilePicture
                             size={50}
-                            url={item.rider.photoUrl}
+                            url={otherUser.photoUrl}
                         />
-                    );
-                }}
-                onPress={() => props.navigation.push("Profile", { id: item.rider.id, beep: item.id })}
-                title={`You beeped ${item.rider.first} ${item.rider.last}`}
+                    )
+                }
+                onPress={() => props.navigation.push("Profile", { id: otherUser.id, beep: item.id })}
+                title={
+                    user.id === item.rider.id ?
+                        `${otherUser.name} beeped you`
+                        :
+                        `You beeped ${otherUser.name}`
+                }
                 description={`Group size: ${item.groupSize}\nOrigin: ${item.origin}\nDestination: ${item.destination}\nDate: ${new Date(item.start)}`}
             />
         );
-        
-        if (!loading) {
-            if (data?.getBeepHistory && data.getBeepHistory.items.length > 0) {
-                return (
+    };
+
+    if (!loading) {
+        if (data?.getBeeps && data.getBeeps.items.length > 0) {
+            return (
+                <>
+                <TopNavigation
+                    title='Beep Logs' 
+                    subtitle={`${data.getBeeps.count} beeps`}
+                    alignment='center' 
+                    accessoryLeft={BackAction} 
+                />
                 <Layout style={styles.container}>
                     <List
                         style={{width:"100%"}}
-                        data={data?.getBeepHistory.items}
+                        data={data?.getBeeps.items}
                         ItemSeparatorComponent={Divider}
                         renderItem={renderItem}
                     />
                 </Layout>
-                );
-            }
-            else {
-                return (
-                    <Layout style={styles.container}>
-                        <Text category='h5'>Nothing to display!</Text>
-                        <Text appearance='hint'>You have no previous beeps to display</Text>
-                    </Layout>
-                );
-            }
+                </>
+            );
         }
         else {
             return (
                 <Layout style={styles.container}>
-                    <Text category='h5'>Loading your history</Text>
-                    <Spinner />
+                    <Text category='h5'>Nothing to display!</Text>
+                    <Text appearance='hint'>You have no previous beeps to display</Text>
                 </Layout>
             );
         }
+    }
+    else {
+        return (
+            <Layout style={styles.container}>
+                <Text category='h5'>Loading your history</Text>
+                <Spinner />
+            </Layout>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
-        height: '83%',
+        height: '100%',
         alignItems: "center",
         justifyContent: 'center'
     }
--- app/routes/ratings/RatingsOnMe.tsx
+++ app/routes/ratings/RatingsOnMe.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Layout, Text, Divider, List, ListItem, Spinner } from '@ui-kitten/components';
+import React, {useContext} from 'react';
+import { Layout, Text, Divider, List, ListItem, Spinner, TopNavigation, TopNavigationAction } from '@ui-kitten/components';
 import { StyleSheet } from 'react-native';
 import ProfilePicture from '../../components/ProfilePicture';
 import {BottomTabNavigationProp} from '@react-navigation/bottom-tabs';
 import {MainNavParamList} from '../../navigators/MainTabs';
 import { gql, useQuery } from '@apollo/client';
-import {GetRatingsOnMeQuery} from '../../generated/graphql';
+import { GetRatingsQuery, Rating } from '../../generated/graphql';
 import {printStars} from '../../components/Stars';
+import {BackIcon} from '../../utils/Icons';
+import {UserContext} from '../../utils/UserContext';
 
 interface Props {
     navigation: BottomTabNavigationProp<MainNavParamList>;
 }
 
 const GetRatingsOnMe = gql`
-    query GetRatingsOnMe($id: String, $me: Boolean) {
-        getRatings(id: $id, me: $me) {
+    query GetRatings($id: String) {
+        getRatings(id: $id) {
             items {
                 id
                 stars
@@ -25,66 +27,90 @@ const GetRatingsOnMe = gql`
                     name
                     photoUrl
                 }
+                rated {
+                    id
+                    name
+                    photoUrl
+                }
             }
             count
         }
     }
 `;
 
-export function RatingsOnMeScreen(props: Props) {
-    const { data, loading, error, refetch } = useQuery<GetRatingsOnMeQuery>(GetRatingsOnMe, { variables: { me: true } });
+export function RatingsScreen(props: Props) {
+    const { data, loading, error, refetch } = useQuery<GetRatingsQuery>(GetRatingsOnMe, { variables: { me: true } });
+    const user = useContext(UserContext);
+    const BackAction = () => (
+        <TopNavigationAction icon={BackIcon} onPress={() => props.navigation.goBack()}/>
+    );
 
-        const renderItem = ({ item }) => (
+    const renderItem = ({ item } : { item: Rating }) => {
+        const otherUser = user.id === item.rater.id ? item.rated : item.rater;
+        return (
             <ListItem
-                accessoryLeft={() => {
-                    return (
-                        <ProfilePicture
-                            size={50}
-                            url={item.rater.photoUrl}
-                        />
-                    );
-                }}
-                onPress={() => props.navigation.push("Profile", { id: item.rater.id })}
-                title={`${item.rater.name} rated you`}
+                accessoryLeft={() => (
+                    <ProfilePicture
+                        size={50}
+                        url={otherUser.photoUrl}
+                    />
+                )
+                }
+                onPress={() => props.navigation.push("Profile", { id: otherUser.id })}
+                title={
+                    user.id === item.rater.id ?
+                    `You rated ${otherUser.name}`
+                    :
+                    `${otherUser.name} rated you`
+                }
                 description={`Message: ${item.message || "N/A"}\nStars: ${printStars(item.stars)} ${item.stars}\n`}
             />
         );
-        
-        if (!loading) {
-            if (data?.getRatings?.items && data.getRatings.count != 0) {
-                return (
-                <Layout style={styles.container}>
-                    <List
-                        style={{width:"100%"}}
-                        data={data?.getRatings.items}
-                        ItemSeparatorComponent={Divider}
-                        renderItem={renderItem}
+    };
+
+    if (!loading) {
+        if (data?.getRatings?.items && data.getRatings.count != 0) {
+            return (
+                <>
+                    <TopNavigation
+                        title='Ratings' 
+                        subtitle={`${data.getRatings.count} ratings`}
+                        alignment='center' 
+                        accessoryLeft={BackAction} 
                     />
-                </Layout>
-                );
-            }
-            else {
-                return (
                     <Layout style={styles.container}>
-                        <Text category='h5'>Nothing to display!</Text>
-                        <Text appearance='hint'>You have no ratings</Text>
+                        <List
+                            style={{width:"100%"}}
+                            data={data?.getRatings.items}
+                            ItemSeparatorComponent={Divider}
+                            renderItem={renderItem}
+                        />
                     </Layout>
-                );
-            }
+                </>
+            );
         }
         else {
             return (
                 <Layout style={styles.container}>
-                    <Text category='h5'>Loading your ratings</Text>
-                    <Spinner />
+                    <Text category='h5'>Nothing to display!</Text>
+                    <Text appearance='hint'>You have no ratings</Text>
                 </Layout>
             );
         }
+    }
+    else {
+        return (
+            <Layout style={styles.container}>
+                <Text category='h5'>Loading your ratings</Text>
+                <Spinner />
+            </Layout>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
-        height: '83%',
+        height: '100%',
         alignItems: "center",
         justifyContent: 'center'
     }
